76016f31417fe4574b041ad6ca112b4419afb3b5
nik.kuzivanov13 <nik.kuzivanov13@gmail.com>
2025-04-11 01:08:44 +0000

    Витя

---
+++ vite.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    base: './',
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "client/src"),
+        "@shared": "./shared",
+        "@assets": path.resolve(__dirname, "attached_assets"),
+      },
+    },
+    root: path.resolve(__dirname, "client"),
+    build: {
+      outDir: path.resolve(__dirname, "dist/public"),
+      assetsDir: "assets",
+      emptyOutDir: true,
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true
+        }
+      }
+    },
+    publicDir: "client/public"
+  }
+})
