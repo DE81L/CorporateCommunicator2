c4721aa26ed140270d9d7edaa03efe723d08ec62
nik.kuzivanov13 <nik.kuzivanov13@gmail.com>
2025-04-12 02:45:47 +0000

    a

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -67,24 +67,24 @@ function App() {
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <LanguageProvider>
-            <Toaster />
-              <EnvironmentIndicator />
-              <div className="flex flex-col h-screen">
-                {/* Only show window frame in Electron */}
-                {isElectron && <WindowFrame />}
-                {/* Main content area with conditional padding */}
-                <div className={`flex-1 overflow-auto ${isElectron ? "pt-0" : ""}`}>
-                  <Router />
-                </div>
-              </div>
+    <LanguageProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Toaster />
+          <EnvironmentIndicator />
+          <div className="flex flex-col h-screen">
+            {/* Only show window frame in Electron */}
+            {isElectron && <WindowFrame />}
+            {/* Main content area with conditional padding */}
+            <div
+              className={`flex-1 overflow-auto ${isElectron ? "pt-0" : ""}`}
+            >
+              <Router />
             </div>
-            
+          </div>
+        </AuthProvider>
+      </QueryClientProvider>
       </LanguageProvider>
-      </AuthProvider>
-    </QueryClientProvider>
   );
 }
 
