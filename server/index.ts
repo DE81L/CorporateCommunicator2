d214af442f005b592c9aa388b5b5d61bf921d597
nik.kuzivanov13 <nik.kuzivanov13@gmail.com>
2025-04-13 16:48:52 +0000

    index.ts changes

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,8 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import { createApp } from './app';
 import http from 'http';
 import { db, connectToDb } from './db'; // Updated import here
