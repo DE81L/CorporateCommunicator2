f91d57a0473bca79bf3ccf98c5681726e3a92f47
nik.kuzivanov13 <nik.kuzivanov13@gmail.com>
2025-04-13 13:47:11 +0000

    qad fix

--- shared/electron-shared/electron-server-interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ElectronServerInterface {
-  connectToDb: () => Promise<void>;
-  setupAuth: () => Promise<void>;
-  registerRoutes: () => Promise<void>;
-  setupVite: () => Promise<void>;
-  serveStatic: (path: string) => any;
-}
