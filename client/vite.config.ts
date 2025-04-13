a8c64e01ffa924bac3da7f2c4545f30cef366db8
nik.kuzivanov13 <nik.kuzivanov13@gmail.com>
2025-04-13 20:32:25 +0000

    dead

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -22,12 +22,6 @@ export default defineConfig(({ mode }) => {
               }
               res.end(JSON.stringify({ error: 'proxy_error', message: err.message }));
             });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request to the Target:', req.method, req.url);
-            });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-            });
           },
         },
       },
