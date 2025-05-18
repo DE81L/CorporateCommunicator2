83e6f411405f19cf3cb6ef026f1ea052c0cfbb58
DE81L <86817084+DE81L@users.noreply.github.com>
2025-05-19 04:04:09 +0900

    Reset online status on startup

--- server/src/db.ts
+++ server/src/db.ts
@@ -27,4 +27,17 @@ export async function connectDb(): Promise<void> {
   }
 }
 
+export async function resetAllOnlineStatus(): Promise<void> {
+  if (!pgClient) {
+    throw new Error('Database not connected');
+  }
+  try {
+    await pgClient.query('UPDATE users SET isonline = 0');
+    logger.info('All user statuses reset to offline');
+  } catch (err) {
+    logger.error('Failed to reset user statuses:', err);
+    throw err;
+  }
+}
+
 export { pgClient as db };
