2fbec1a6527aed8634906d61649848d8eb15dd45
DE81L <86817084+DE81L@users.noreply.github.com>
2025-05-19 13:27:59 +0900

    Implement requests fetch and fix message handler

--- client/src/pages/messages-section.tsx
+++ client/src/pages/messages-section.tsx
@@ -299,6 +299,7 @@ export default function MessagesSection({ onStartCall }: Props) {
             file: fileData,
           }),
         });
+        if (!saved) return;
 
         console.info('Message sent to server', {
           from: user!.id,
