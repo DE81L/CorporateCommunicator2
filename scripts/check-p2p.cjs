489963cb41008206635e46ba714ba48f693c9edd
DE81L <86817084+DE81L@users.noreply.github.com>
2025-05-22 09:19:39 +0900

    add p2p connection check script

---
+++ scripts/check-p2p.cjs
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const SimplePeer = require('simple-peer');
+
+function createPeer(initiator) {
+  return new SimplePeer({
+    initiator,
+    trickle: false,
+    config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] },
+  });
+}
+
+const peer1 = createPeer(true);
+const peer2 = createPeer(false);
+
+peer1.on('signal', data => peer2.signal(data));
+peer2.on('signal', data => peer1.signal(data));
+
+let success = false;
+
+peer1.on('connect', () => {
+  console.log('peer1 connected');
+  peer1.send('ping');
+});
+
+peer2.on('data', data => {
+  console.log('peer2 got:', data.toString());
+  if (data.toString() === 'ping') {
+    peer2.send('pong');
+  }
+});
+
+peer1.on('data', data => {
+  console.log('peer1 got:', data.toString());
+  if (data.toString() === 'pong') {
+    success = true;
+    console.log('P2P connection OK');
+    cleanup();
+  }
+});
+
+const timeout = setTimeout(() => {
+  console.error('P2P connection failed');
+  cleanup();
+}, 5000);
+
+function cleanup() {
+  clearTimeout(timeout);
+  peer1.destroy();
+  peer2.destroy();
+  if (!success) process.exit(1);
+}
